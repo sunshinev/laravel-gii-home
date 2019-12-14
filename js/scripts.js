d66b8535db28875c293a03035412de51ee3cd1a2
jaysun <wx_97b8e53e64034716b197bc1623aa8c53@git.code.tencent.com>
2019-12-15 00:47:18 +0800

    test

--- js/scripts.js
+++ js/scripts.js
@@ -9,16 +9,16 @@
     "use strict"; 
 	
 	/* Preloader */
-	$(window).on('load', function() {
-		var preloaderFadeOutTime = 500;
-		function hidePreloader() {
-			var preloader = $('.spinner-wrapper');
-			setTimeout(function() {
-				preloader.fadeOut(preloaderFadeOutTime);
-			}, 500);
-		}
-		hidePreloader();
-	});
+	// $(window).on('load', function() {
+	// 	var preloaderFadeOutTime = 10;
+	// 	function hidePreloader() {
+	// 		var preloader = $('.spinner-wrapper');
+	// 		setTimeout(function() {
+	// 			preloader.fadeOut(preloaderFadeOutTime);
+	// 		}, 5);
+	// 	}
+	// 	hidePreloader();
+	// });
 
 	
 	/* Navbar Scripts */
